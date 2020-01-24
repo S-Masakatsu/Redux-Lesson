de743edf216d418ca18da22bb429f23ebfb8ec21
S-Masakatsu <s.masakatsu197@gmail.com>
2020-01-24 11:24:17 +0900

    Updates

---
+++ precache-manifest.bc68e4f2131fd5fc78d84511eeebc20f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804dbfe4f43afb0c1eacceb0e39d33b2",
+    "url": "/Redux-Lesson/index.html"
+  },
+  {
+    "revision": "b18307f0705799c33dd8",
+    "url": "/Redux-Lesson/static/css/main.ebf15299.chunk.css"
+  },
+  {
+    "revision": "4962bbb29b91051f4549",
+    "url": "/Redux-Lesson/static/js/2.1e82e9a1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Redux-Lesson/static/js/2.1e82e9a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b18307f0705799c33dd8",
+    "url": "/Redux-Lesson/static/js/main.0649c7b5.chunk.js"
+  },
+  {
+    "revision": "fc45e8ef319d8d374053",
+    "url": "/Redux-Lesson/static/js/runtime-main.fd10795a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee2ed116dc93aeff4d99a123fa3144d7.js"
+  "/Redux-Lesson/precache-manifest.bc68e4f2131fd5fc78d84511eeebc20f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Redux-Lesson/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
